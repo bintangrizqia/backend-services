2a60edb3410ac01f31d061a803c7cf72e38354de
Helmy Fauzi <helmyfauzi@pindad.com>
2025-04-23 15:54:26 +0700

    update filename

--- sync.sh
+++ sync.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Jalankan semua sync script secara berurutan
-npx tsx ./src/scripts/sync:master:type:unit.ts
-npx tsx ./src/scripts/sync:master:type:position.ts
-npx tsx ./src/scripts/sync:master:position.ts
-npx tsx ./src/scripts/sync:master:organization.ts
-npx tsx ./src/scripts/sync:master:personnel.ts
+npx tsx ./src/scripts/sync-master-type-position.ts
+npx tsx ./src/scripts/sync-master-position.ts
+npx tsx ./src/scripts/sync-master-type-unit.ts
+npx tsx ./src/scripts/sync-master-organization.ts
+npx tsx ./src/scripts/sync-master-personnel.ts
