a7d8c81f1c347bdb6c7def2d4ec3b5c66c09b3f5
Helmy Fauzi <helmyfauzi@pindad.com>
2025-05-09 11:14:30 +0700

    [Update] urutan sync

--- sync.sh
+++ sync.sh
@@ -5,14 +5,14 @@ set -e  # Stop on any error
 echo "Running master type position..."
 npx tsx ./src/scripts/sync-master-type-position.ts
 echo "Done"
-echo "Running master position..."
-npx tsx ./src/scripts/sync-master-position.ts
+echo "Running master organization..."
+npx tsx ./src/scripts/sync-master-organization.ts
 echo "Done"
 echo "Running master type unit..."
 npx tsx ./src/scripts/sync-master-type-unit.ts
 echo "Done"
-echo "Running master organizations"
-npx tsx ./src/scripts/sync-master-organization.ts
+echo "Running master position"
+npx tsx ./src/scripts/sync-master-position.ts
 echo "Done"
 echo "Running master personnels..."
 npx tsx ./src/scripts/sync-master-personnel.ts
